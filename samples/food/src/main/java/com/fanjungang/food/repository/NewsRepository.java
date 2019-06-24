ca11d9ae4efef7bc571479fc51b8f71750983f58
zixian5 <406964409@qq.com>
2019-06-24 12:52:09 +0800

    add recover

--- samples/food/src/main/java/com/fanjungang/food/repository/NewsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fanjungang.food.repository;
-
-import com.fanjungang.food.model.News;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NewsRepository  extends JpaRepository<News, Integer> {
-}
